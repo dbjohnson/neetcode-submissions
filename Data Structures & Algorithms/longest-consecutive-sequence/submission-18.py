e1f8d9a6d8f56e9f4912e28884e0b291ad4a7939
Bryan Johnson <d.bryan.johnson@gmail.com>
2026-08-09 14:16:10 -0400

    Add: longest-consecutive-sequence - submission-18

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-18.py
@@ -0,0 +1,28 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        uniq = set(nums)
+        counts = dict()
+        
+        def consecutive(n):
+            if n - 1 in uniq:
+                return 0
+                
+            c = 0
+            nn = n
+            while nn in uniq:
+                if nn in counts:
+                    c += counts[nn]
+                    break
+                c += 1
+                nn += 1
+
+            counts[n] = c
+            return c
+
+        return max(
+            consecutive(n) 
+            for n in uniq
+        )
