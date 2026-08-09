48f1d459ae83979afbe63318681f6ab674c37879
Bryan Johnson <d.bryan.johnson@gmail.com>
2026-08-08 22:07:46 -0400

    Add: string-encode-and-decode - submission-14

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-14.py
@@ -0,0 +1,24 @@
+from itertools import combinations
+from string import printable
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return '-'
+
+        for count in range(1, 3):
+            for chars in combinations(printable, count):
+                delim = "".join(chars)
+                for s2 in strs:
+                    if delim in s2:
+                        break
+                else:
+                    return f"{delim}|" + delim.join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        if s == '-':
+            return []
+        delim, strings = s.split("|", maxsplit=1)
+        return strings.split(delim)
