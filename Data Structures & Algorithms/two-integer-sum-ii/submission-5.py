10010b0c7a97fa950ffdd550b2b31b8358c14305
Bryan Johnson <d.bryan.johnson@gmail.com>
2026-08-09 20:32:08 -0400

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        uniq = set(numbers)
+        for i, n1 in enumerate(numbers):
+            if target - n1 in uniq:
+                try:
+                    j = numbers[i + 1:].index(target - n1) + i + 1
+                    return [i + 1, j + 1]
+                except ValueError:
+                    pass
