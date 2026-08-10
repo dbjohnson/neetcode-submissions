500366cff77b6f692ed46688f555371056e2f973
Bryan Johnson <d.bryan.johnson@gmail.com>
2026-08-10 19:35:33 -0400

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        heightmap = defaultdict(list)
+        
+        for i, h in enumerate(height):
+            for hh in range(1, h + 1):
+                heightmap[hh].append(i)
+
+        trapped = sum(
+            (idxs[-1] - idxs[0] + 1)
+            for h, idxs in heightmap.items()
+        ) - sum(height)
+        return trapped
+
